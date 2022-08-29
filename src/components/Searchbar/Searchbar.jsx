c98342b8ce4ed1fb6c1b2ed071a0ebe0f96fcc15
Bohdan Budzyk <webhawky@MacBook-Pro-Bohdan.local>
2022-08-29 21:18:33 +0300

    refactored searchbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,56 +1,52 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FcSearch } from 'react-icons/fc';
 
 import s from './Searchbar.module.scss';
 
-export default class Searchbar extends Component {
-  state = {
-    searchPhotoName: '',
-  };
+export default function Searchbar({ onSubmit }) {
+  const [searchPhotoName, setSearchPhotoName] = useState('');
 
-  changeInputValueHandler = e => {
+  const changeInputValueHandler = e => {
     const { value } = e.target;
 
-    this.setState({ searchPhotoName: value.toLowerCase() });
+    setSearchPhotoName(value.toLowerCase());
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    if (this.state.searchPhotoName.trim() === '') {
+    if (searchPhotoName.trim() === '') {
       alert('Input field can not be empty');
       return;
     }
 
-    this.props.onSubmit(this.state.searchPhotoName);
-    this.setState({ searchPhotoName: '' });
+    onSubmit(searchPhotoName);
+    searchPhotoName('');
   };
 
-  render() {
-    return (
-      <>
-        <header className={s.Searchbar} onSubmit={this.handleSubmit}>
-          <form className={s.SearchForm}>
-            <button type="submit" className={s.SearchForm_button}>
-              <FcSearch size={28} />
-              <span className={s.SearchForm_button_label}>Search</span>
-            </button>
+  return (
+    <>
+      <header className={s.Searchbar} onSubmit={handleSubmit}>
+        <form className={s.SearchForm}>
+          <button type="submit" className={s.SearchForm_button}>
+            <FcSearch size={28} />
+            <span className={s.SearchForm_button_label}>Search</span>
+          </button>
 
-            <input
-              className={s.SearchForm_input}
-              type="text"
-              value={this.state.searchPhotoName}
-              onChange={this.changeInputValueHandler}
-              autoComplete="off"
-              autoFocus
-              placeholder="Search images and photos"
-            />
-          </form>
-        </header>
-      </>
-    );
-  }
+          <input
+            className={s.SearchForm_input}
+            type="text"
+            value={searchPhotoName}
+            onChange={changeInputValueHandler}
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    </>
+  );
 }
 
 Searchbar.propTypes = {
